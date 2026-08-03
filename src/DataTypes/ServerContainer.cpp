0e0ef30ba713a455e5e2380455712f6c0cccd880
Matthew Ackroyd <matt.ackroyd04@gmail.com>
2026-08-03 15:27:04 -0600

    Actualy Fixed it this time

--- src/DataTypes/ServerContainer.cpp
+++ src/DataTypes/ServerContainer.cpp
@@ -22,8 +22,8 @@ void ServerContainer::serialize() {
     memcpy(data+offset, uuid, UUID_BYTE_SIZE);
     offset += UUID_BYTE_SIZE;
 
-    // Connected User info (offset by length of datatypes because we already know this is a user)
-    memcpy(data+offset, this->serverUser.getData()+sizeof(DataTypes), this->serverUser.getDataLen()-sizeof(DataTypes));
+    // Connected User info (offset by length of datatypes because we already know this is a)
+    memcpy(data+offset+sizeof(DataTypes), this->serverUser.getData(), this->serverUser.getDataLen()-sizeof(DataTypes));
     offset += this->serverUser.getDataLen();
 }
 
